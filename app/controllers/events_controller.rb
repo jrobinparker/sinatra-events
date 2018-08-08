a181dd515acc2dc2c3ec806376df8e2418162e9a
Jeremy Parker <jrobinparker@gmail.com>
2018-08-08 16:27:06 +0000

    Added edit and delete functions to Events controller.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,41 @@
 class EventsController < ApplicationController
-end 
+
+  get '/' do
+    @events = Event.all
+    erb :"events/index"
+  end
+
+  get '/new' do
+    erb :"events/new"
+  end
+
+  post '/' do
+    @event = Event.create(params[:event])
+    @event.save
+    redirect to "events/#{@event.id}"
+  end
+
+  get '/:id' do
+    @event = Event.find_by_id(params[:id])
+    erb :"events/show"
+  end
+
+  get '/:id/edit' do
+    @event = Event.find_by_id(params[:id])
+    erb :"events/edit"
+  end
+
+  post '/:id' do
+    @event = Event.find_by_id(params[:id])
+    @event = Event.update(params[:event])
+    @event.save
+    redirect to "events/#{@event.id}"
+  end
+
+  delete '/:id/delete' do
+    @event = Event.find_by_id(params[:id])
+    @event.delete
+    redirect to "events"
+  end 
+
+end
