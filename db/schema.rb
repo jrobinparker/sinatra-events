15505b263fbe2e1440af831960a03f0126abc7f9
Jeremy Parker <jrobinparker@gmail.com>
2018-08-08 19:50:16 +0000

    Added user id to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_174049) do
+ActiveRecord::Schema.define(version: 2018_08_08_194828) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_08_08_174049) do
     t.string "end_date"
     t.string "end_time"
     t.string "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
