c2de411945b4eaf178d281c8a6e612351b9353ca
Jeremy Parker <jrobinparker@gmail.com>
2018-08-08 18:07:20 +0000

    Added signup form and user home.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,54 +1,41 @@
 class UsersController < ApplicationController
 
   get '/' do
-  erb :'users/login'
-end
-
-get "/signup" do
-  if logged_in?
-    redirect to "events"
-  else
-    erb :'users/signup'
+    @user = User.find_by_id(params[:id])
+    erb :'users/index'
   end
-end
 
-post "/signup" do
- if params[:username] == "" || params[:email] == "" || params[:password] == "" || params[:first_name] == "" || params[:last_name] == ""
-   redirect 'home/signup'
- else
-   @user = User.create(username: params[:username], password: params[:password], email: params[:email], first_name: params[:first_name], last_name: params[:last_name])
-   if @user && @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    redirect to "events"
+  get "/signup" do
+      erb :'users/signup'
   end
- end
-end
 
-get "/login" do
-  if logged_in?
-    redirect to "events"
-  else
-    erb :'users/login'
+  post "/" do
+      @user = User.create(username: params[:username], password: params[:password], email: params[:email], first_name: params[:first_name], last_name: params[:last_name])
+      @user.save
+      redirect to "/events"
   end
-end
 
-post "/login" do
-  @user = User.find_by(username: params[:username])
-  if @user && @user.authenticate(params[:password])
-   session[:user_id] = @user.id
-   redirect to "events"
-  else
-   redirect to "users/failure"
+  get "/login" do
+      erb :'users/login'
   end
-end
 
-get "/failure" do
- erb :'home/failure'
-end
+  post "/login" do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "events"
+    else
+      redirect to "users/failure"
+    end
+  end
 
-get "/logout" do
- session.clear
- redirect "users/login"
-end
+  get "/failure" do
+    erb :'home/failure'
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "users/login"
+  end
 
 end
--- config.ru
+++ config.ru
@@ -2,9 +2,11 @@ require './config/environment'
 require 'sinatra'
 
 require_relative 'app/controllers/events_controller'
+require_relative 'app/controllers/users_controller'
 require_relative 'app/controllers/application_controller'
 
 use Rack::MethodOverride
 
 map('/events') { run EventsController }
+map('/users') { run UsersController }
 map('/') { run ApplicationController }
