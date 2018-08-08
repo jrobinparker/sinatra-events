f610efc6cd464556455e9123c380717eb4c3f75c
Jeremy Parker <jrobinparker@gmail.com>
2018-08-08 19:59:07 +0000

    Moved signup and login actions to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,64 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "events"
   end
 
-  get "/" do
-    erb :index
+  get '/' do
+    erb :'home/home'
   end
+
+  get "/signup" do
+    if logged_in?
+      redirect to "events"
+    else
+      erb :'home/signup'
+    end
+  end
+
+  post "/signup" do
+    if params[:username] == "" || params[:email] == "" || params[:password] == "" || params[:first_name] == "" || params[:last_name] == ""
+      redirect 'home/signup'
+    else
+      @user = User.create(username: params[:username], password: params[:password], email: params[:email], first_name: params[:first_name], last_name: params[:last_name])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect to "events"
+      end
+    end
+  end
+
+  get "/login" do
+    if logged_in?
+      redirect to "events"
+    else
+      erb :'home/login'
+    end
+  end
+
+  post "/login" do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "events"
+    else
+      redirect to "home/failure"
+    end
+  end
+
+  get "/failure" do
+    erb :'home/failure'
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "home/login"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      @user = User.find(session[:user_id])
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,37 +5,4 @@ class UsersController < ApplicationController
     erb :'users/index'
   end
 
-  get "/signup" do
-      erb :'users/signup'
-  end
-
-  post "/" do
-      @user = User.create(username: params[:username], password: params[:password], email: params[:email], first_name: params[:first_name], last_name: params[:last_name])
-      @user.save
-      redirect to "/events"
-  end
-
-  get "/login" do
-      erb :'users/login'
-  end
-
-  post "/login" do
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect to "events"
-    else
-      redirect to "users/failure"
-    end
-  end
-
-  get "/failure" do
-    erb :'home/failure'
-  end
-
-  get "/logout" do
-    session.clear
-    redirect "users/login"
-  end
-
 end
