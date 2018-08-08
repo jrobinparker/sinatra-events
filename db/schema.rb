b03c0105d9f1d33b81b4ce6f50914a164ac4b571
Jeremy Parker <jrobinparker@gmail.com>
2018-08-08 17:43:25 +0000

    Created user model and migration. Created relationship between events and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_160402) do
+ActiveRecord::Schema.define(version: 2018_08_08_174049) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2018_08_08_160402) do
     t.string "end_date"
     t.string "end_time"
     t.string "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "event_id"
   end
 
 end
